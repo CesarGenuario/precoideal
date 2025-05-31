6a4d89df1d8ab4da17d40f25c4d4035fc4e62b41
CesarGenuario <cesaricard@gmail.com>
2025-05-31 18:26:23 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import VehicleLookup from './VehicleLookup'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col justify-start items-center p-4">
+      <VehicleLookup />
+    </div>
+  )
+}
